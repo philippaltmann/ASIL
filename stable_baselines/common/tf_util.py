281243d594f2e94aa33d2811b837eb6c4f2b5a43
Philipp <philippaltmann@deap.io>
2020-07-23 15:52:19 +0200

    Added optional tag to total_episode_reward_logger

--- stable_baselines/common/tf_util.py
+++ stable_baselines/common/tf_util.py
@@ -479,7 +479,7 @@ def outer_scope_getter(scope, new_scope=""):
 # ================================================================
 
 
-def total_episode_reward_logger(rew_acc, rewards, masks, writer, steps):
+def total_episode_reward_logger(rew_acc, rewards, masks, writer, steps, tag="rewards/episode_reward"):
     """
     calculates the cumulated episode reward, and prints to tensorflow log the output
 
@@ -488,7 +488,6 @@ def total_episode_reward_logger(rew_acc, rewards, masks, writer, steps):
     :param masks: (np.array bool) the end of episodes
     :param writer: (TensorFlow Session.writer) the writer to log to
     :param steps: (int) the current timestep
-    :return: (np.array float) the updated total running reward
     :return: (np.array float) the updated total running reward
     """
     with tf.variable_scope("environment_info", reuse=True):
@@ -499,12 +498,45 @@ def total_episode_reward_logger(rew_acc, rewards, masks, writer, steps):
                 rew_acc[env_idx] += sum(rewards[env_idx])
             else:
                 rew_acc[env_idx] += sum(rewards[env_idx, :dones_idx[0, 0]])
-                summary = tf.Summary(value=[tf.Summary.Value(tag="episode_reward", simple_value=rew_acc[env_idx])])
+                summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=rew_acc[env_idx])])
                 writer.add_summary(summary, steps + dones_idx[0, 0])
                 for k in range(1, len(dones_idx[:, 0])):
                     rew_acc[env_idx] = sum(rewards[env_idx, dones_idx[k - 1, 0]:dones_idx[k, 0]])
-                    summary = tf.Summary(value=[tf.Summary.Value(tag="episode_reward", simple_value=rew_acc[env_idx])])
+                    summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=rew_acc[env_idx])])
                     writer.add_summary(summary, steps + dones_idx[k, 0])
                 rew_acc[env_idx] = sum(rewards[env_idx, dones_idx[-1, 0]:])
 
     return rew_acc
+
+# Reward Logger \w tag
+# def total_episode_reward_logger(rew_acc, rewards, masks, writer, steps, tag="rewards/episode_reward"):
+#     """
+#     calculates the cumulated episode reward, and prints to tensorflow log the output
+#
+#     :param rew_acc: (np.array float) the total running reward
+#     :param rewards: (np.array float) the rewards
+#     :param masks: (np.array bool) the end of episodes
+#     :param writer: (TensorFlow Session.writer) the writer to log to
+#     :param steps: (int) the current timestep
+#     :return: (np.array float) the updated total running reward
+#     :return: (np.array float) the updated total running reward
+#     """
+#     # from stable_baselines import logger
+#     # logger.log("In Episode Logger ({})\n\trew_acc: {}\n\trewards: {}\n\tmasks: {}\n\tsteps: {}".format(tag, rew_acc, rewards, masks, steps))
+#     with tf.variable_scope("environment_info", reuse=True):
+#         for env_idx in range(rewards.shape[0]):
+#             dones_idx = np.sort(np.argwhere(masks[env_idx]))
+#
+#             if len(dones_idx) == 0:
+#                 rew_acc[env_idx] += sum(rewards[env_idx])
+#             else:
+#                 rew_acc[env_idx] += sum(rewards[env_idx, :dones_idx[0, 0]])
+#                 summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=rew_acc[env_idx])])
+#                 writer.add_summary(summary, steps + dones_idx[0, 0])
+#                 for k in range(1, len(dones_idx[:, 0])):
+#                     rew_acc[env_idx] = sum(rewards[env_idx, dones_idx[k - 1, 0]:dones_idx[k, 0]])
+#                     summary = tf.Summary(value=[tf.Summary.Value(tag=tag, simple_value=rew_acc[env_idx])])
+#                     writer.add_summary(summary, steps + dones_idx[k, 0])
+#                 rew_acc[env_idx] = sum(rewards[env_idx, dones_idx[-1, 0]:])
+#
+#     return rew_acc
