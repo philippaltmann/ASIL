ea2ab293b3bf509a74877657c07aa348d0a3bf77
Philipp <philippaltmann@deap.io>
2020-07-29 02:52:15 +0200

    Removed SIL dependency

--- stable_baselines/__init__.py
+++ stable_baselines/__init__.py
@@ -9,7 +9,7 @@ from stable_baselines.ppo2 import PPO2
 from stable_baselines.td3 import TD3
 from stable_baselines.sac import SAC
 
-from stable_baselines.sil import SIL
+# from stable_baselines.sil import SIL
 from stable_baselines.asil import ASIL
 from stable_baselines.gasil import GASIL
 
